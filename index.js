ad1dbeced5da315d5e1d32f999ac0eb6a55ee928
Vanessa720 <vanessawilliamson718@gmail.com>
2021-04-01 20:46:44 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let main = document.getElementById('main');
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "Vanessa is the champion!";
